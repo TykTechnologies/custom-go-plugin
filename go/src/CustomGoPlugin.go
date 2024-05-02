3953e935d91d6af66065eb7f2f7237eff2079e32
Sedky <sedkyaboushamalah@gmail.com>
2024-05-02 14:22:40 -0400

    bump to v5.3.0 and migrate to OAS API, add config K/V example + metadata example

--- go/src/CustomGoPlugin.go
+++ go/src/CustomGoPlugin.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 
 	"github.com/TykTechnologies/opentelemetry/trace"
+	"github.com/TykTechnologies/tyk/apidef/oas"
 	"github.com/TykTechnologies/tyk/ctx"
 
 	"github.com/TykTechnologies/tyk/log"
@@ -26,7 +28,7 @@ func AddFooBarHeader(rw http.ResponseWriter, r *http.Request) {
 	// Set the status of the span.
 	newSpan.SetStatus(trace.SPAN_STATUS_OK, "")
 
-	r.Header.Add("Foo", "Bar")
+	r.Header.Add("X-SimpleHeader-Inject", "foo")
 }
 
 // Custom Auth, applies a rate limit of
@@ -61,8 +63,64 @@ func AuthCheck(rw http.ResponseWriter, r *http.Request) {
 	ctx.SetSession(r, session, true)
 }
 
+// Injects meta data from a token where the metadata key is "foo"
+func InjectMetadata(rw http.ResponseWriter, r *http.Request) {
+	session := ctx.GetSession(r)
+	if session != nil {
+		// Access session fields such as MetaData
+		metaData := session.MetaData
+		foo, ok := metaData["foo"].(string) // Type assert foo to string
+		if !ok {
+			// Handle the case where foo is not a string or foo does not exist
+			logger.Error("Error: 'foo' is not a string or not found in metaData")
+			return // or continue, depending on your error handling strategy
+		}
+		// Process metaData as needed
+		r.Header.Add("X-Metadata-Inject", foo)
+	}
+}
+
+// Injects config data, both from an env variable and hard-coded
+func InjectConfigData(rw http.ResponseWriter, r *http.Request) {
+	oasDef := ctx.GetOASDefinition(r)
+
+	 // Extract the middleware section safely
+	 xTyk, ok := oasDef.Extensions["x-tyk-api-gateway"].(*oas.XTykAPIGateway)
+	 if !ok {
+		 logger.Println("Middleware extension is missing or invalid.")
+		 return
+	 }
+
+	 configKey := xTyk.Middleware.Global.PluginConfig.Data.Value["env-config-example"].(string)
+	 r.Header.Add("X-ConfigData-Config", configKey)
+}
+
+// Injects config data, both from an env variable and hard-coded
+func MakeOutboundCall(rw http.ResponseWriter, r *http.Request) {
+	// Define the URL
+	url := "https://httpbin.org/get"
+
+	// Create a GET request
+	response, err := http.Get(url)
+	if err != nil {
+		logger.Info("Error making GET request: %s\n", err)
+		return
+	}
+	defer response.Body.Close()
+
+	// Read the response body
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		logger.Info("Error reading response: %s\n", err)
+		return
+	}
+
+	// Print the response body
+	logger.Info(string(responseData))
+}
+
 func main() {}
 
 func init() {
-	logger.Info("--- Go custom plugin v4 init success! ---- ")
+	logger.Info("--- Go custom plugin init success! ---- ")
 }
